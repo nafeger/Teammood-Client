1852008e7146f50c55ae14ac7fd7fb6d80010593
Nathan A. Feger <nafeger@gmail.com>
2020-02-21 10:26:53 -0600

    Add a setup.py so this can be used in requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Teammood-Client',
+   version='1.0',
+   description="Teammood (https://app.teammood.com) is a online app that polls teams daily to see how they are doing and to catch problems before they become big. Geared towards agile dev teams it's a easy way to see how everyone is operating.",
+   author='shaungehring',
+   author_email='shaungehring@me.com',
+   packages=['Teammood-Client'],  #same as name
+   install_requires=['requests'], #external packages as dependencies
+)
